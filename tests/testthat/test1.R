a7cac23cacd9e74ae950f499b3f5661f72be1bb3
Imaduddin Haetami <imaduddin.htm@gmail.com>
2020-05-01 12:27:27 +0000

    Changes in file in progress.

--- tests/testthat/test1.R
+++ tests/testthat/test1.R
@@ -3,7 +3,7 @@ library(data.table)
 library(scales)
 #library(artidata.viz)
 set.seed(240193)
-N=100
+N=10000
 
 dtA=data.table(x=runif(N,-3,3),
                y=runif(N,-3,3),
@@ -22,24 +22,15 @@ dtD=data.table(x=c(rnorm(N/2,-1,.75),rnorm(N/2,1,.75)),
                z="D")
 
 dt1=rbindlist(list(dtA,dtB,dtC,dtD))
-ggsave("plot/minimal100.png",hist2d(dt1,facet=1,hasLine=T),width=30,height=20,units="cm")
-
-ggsave("plot/grey.png",hist2d(dt1,facet=1,theme="grey"),width=30,height=20,units="cm")
-ggsave("plot/dark.png",hist2d(dt1,facet=1,theme="dark"),width=30,height=20,units="cm")
-ggsave("plot/minimal-line.png",hist2d(dt1,facet=1,hasLine=T),width=30,height=20,units="cm")
-ggsave("plot/grey-line.png",hist2d(dt1,facet=1,theme="grey",hasLine=T),width=30,height=20,units="cm")
-ggsave("plot/dark-line.png",hist2d(dt1,facet=1,theme="dark",hasLine=T),width=30,height=20,units="cm")
-ggsave("plot/minimal-grid.png",hist2d(dt1,facet=1,hasGrid=T),width=30,height=20,units="cm")
-ggsave("plot/grey-grid.png",hist2d(dt1,facet=1,theme="grey",hasGrid=T),width=30,height=20,units="cm")
-ggsave("plot/dark-grid.png",hist2d(dt1,facet=1,theme="dark",hasGrid=T),width=30,height=20,units="cm")
 scatter=ggplot(dt1,aes(x=x,y=y))+geom_point(size=0.1)+facet_wrap(vars(z))+theme_minimal()
 ggsave("plot/scatter plot.png",scatter,width=30,height=20,units="cm")
+ggsave("plot/minimal-stderr.png",hist2d(dt1,facet=1,hasLine=T,nBin=100,statsLine=c("rsquare","count","stderr")),width=30,height=20,units="cm")
+
+ggsave("plot/minimal-50.png",hist2d(dt1,facet=1,hasLine=T,nBin=50),width=30,height=20,units="cm")
+ggsave("plot/minimal-100.png",hist2d(dt1,facet=1,hasLine=T,nBin=100),width=30,height=20,units="cm")
 
 
-dtE=data.table(x=rep(seq(-2,2,length.out=N),2))
-dtE[1:N,y:=sqrt(2^2-x^2)]
-dtE[(N+1):(2*N),y:=-sqrt(2^2-x^2)]
-dtE[,":="(x=x+rnorm(2*N),y=y+rnorm(2*N))]
-
-ggplot(data=dtE,aes(x=x,y=y))+
-  geom_point(size=0.001)
+dt2=rbindlist(list(dt1,data.table(x=seq(-1,1,length.out=1000),y=0,z="B")))
+scatter2=ggplot(dt2,aes(x=x,y=y))+geom_point(size=0.1)+facet_wrap(vars(z))+theme_minimal()
+ggsave("plot/scatter plot 2.png",scatter2,width=30,height=20,units="cm")
+ggsave("plot/minimal-100 2.png",hist2d(dt2,facet=1,nBin=100),width=30,height=20,units="cm")
